7372468fcfe4dc60c132dadfe8888dddeeee90dd
nobilis <kevinbaxterperkins@gmail.com>
2025-03-08 12:28:26 -0600

    finished page three, initialized struct

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -6,6 +6,8 @@ const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
 
+const START_ADDR: u16 = 0x200;
+
 pub struct Emu {
     pc: u16,
     ram: [u8; RAM_SIZE],
@@ -17,4 +19,22 @@ pub struct Emu {
     keys: [bool; NUM_KEYS],
     dt: u8,
     st: u8,
+}
+
+impl Emu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0, RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0, STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
+    }
+
 }
